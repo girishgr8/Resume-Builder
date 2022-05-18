28794f468c80dc4940864d3580c7c99a8997d9ef
girishgr8 <girishthatte35@gmail.com>
2022-05-18 20:24:44 +0530

    Heroku Changes

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const options = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("/", (req, res) => {
-	res.sendFile("index.html");
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 // POST route for PDF generation....
