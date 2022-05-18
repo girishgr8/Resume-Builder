acf225f9a32114716453b1f32f37b0bf6267ec75
girishgr8 <girishthatte35@gmail.com>
2022-05-18 18:20:26 +0530

    dependencies updated to latest version

--- documents/index.js
+++ documents/index.js
@@ -336,81 +336,93 @@ module.exports = (values) => {
                 </div>
 			</div>
 			<!--
-            <p class="mb-1">123 My Place Drive</p>
-            <p class="mb-1">Astoria, New York 11105</p>
-			<p class="mb-1">1-800-CALLPLZ</p>
+            <p class="mb-1">Address Line 1</p>
+            <p class="mb-1">Address Line 2</p>
+			<p class="mb-1">City, Country</p>
 			-->
 			<p class="mb-1"><span style="padding-right:10px;"><i class="fas fa-envelope"></i></span>${email}</p>
 			<p class="mb-1"><span style="padding-right:10px;"><i class="fas fa-phone-square-alt"></i></span>${phone}</p>
 			`;
-	if (skill1 != "" || skill1 != null)
+	if (skill1 != "" && skill1 != null)
 		htmlTemplate += `<p class="rela-block caps side-header">Skills</p>
 				<p class="rela-block list-thing">${skill1}</p>`;
-	if (skill2 != "" || skill2 != null)
+	if (skill2 != "" && skill2 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${skill2}</p>`;
-	if (skill3 != "" || skill3 != null)
+	if (skill3 != "" && skill3 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${skill3}</p>`;
-	if (skill4 != "" || skill4 != null)
+	if (skill4 != "" && skill4 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${skill4}</p>`;
-	if (skill5 != "" || skill5 != null)
+	if (skill5 != "" && skill5 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${skill5}</p>`;
-	if (skill6 != "" || skill6 != null)
+	if (skill6 != "" && skill6 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${skill6}</p>`;
 
-	if (interest1 != "" || interest1 != null)
+	if (interest1 != "" && interest1 != null)
 		htmlTemplate += `<p class="rela-block caps side-header">Interests</p>
 				<p class="rela-block list-thing">${interest1}</p>`;
-	if (interest2 != "" || interest2 != null)
+	if (interest2 != "" && interest2 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${interest2}</p>`;
-	if (interest3 != "" || interest3 != null)
+	if (interest3 != "" && interest3 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${interest3}</p>`;
-	if (interest4 != "" || interest4 != null)
+	if (interest4 != "" && interest4 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${interest4}</p>`;
-	if (interest6 != "" || interest6 != null)
+	if (interest6 != "" && interest6 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${interest5}</p>`;
-	if (interest6 != "" || interest6 != null)
+	if (interest6 != "" && interest6 != null)
 		htmlTemplate += `<p class="rela-block list-thing">${interest6}</p>`;
 
 	htmlTemplate += `
   			</div>
-			<div class="rela-block content-container">
-			<!--
-			<h2 class="rela-block caps title">Jr Front-End Developer</h2>
-			<div class="rela-block separator"></div>
-			-->
-			<div class="rela-block caps greyed">Profile</div>
-			<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fas fa-globe"></i></span>${website}</p>
-			<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-github"></i></span>${github}</p>
-			<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-facebook"></i></span>${facebook}</p>
-			<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-twitter-square"></i></span>${twitter}</p>
-			<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-instagram"></i></span>${instagram}</p>
-			<p class="rela-block social mb-4"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-linkedin"></i></span>${linkedin}</p>			
-			
-			
-			<div class="rela-block caps greyed">Education</div>
-            <h3 class="mb-0">${college}</h3>
+			<div class="rela-block content-container">			
+			<div class="rela-block caps greyed">Profile</div>`;
+	if (website != "" && website != null)
+		htmlTemplate += `<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fas fa-globe"></i></span>${website}</p>`;
+	if (github != "" && github != null)
+		htmlTemplate += `<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-github"></i></span>${github}</p>`;
+	if (facebook != "" && facebook != null)
+		htmlTemplate += `<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-facebook"></i></span>${facebook}</p>`;
+	if (twitter != "" && twitter != null)
+		htmlTemplate += `<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-twitter-square"></i></span>${twitter}</p>`;
+	if (instagram != "" && github != instagram)
+		htmlTemplate += `<p class="rela-block social mb-0"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-instagram"></i></span>${instagram}</p>`;
+	if (linkedin != "" && linkedin != null)
+		htmlTemplate += `<p class="rela-block social mb-4"><span style="padding-right: 15px; font-size="20px;"><i class="fab fa-linkedin"></i></span>${linkedin}</p>`;
+
+	htmlTemplate += `<div class="rela-block caps greyed">Education</div>`;
+	if (college != "" && college != null)
+		htmlTemplate += `<h3 class="mb-0">${college}</h3>
 			<p class="text-muted light mt-0 mb-1">${fromyear1}<span class="mx-2">to</span>${toyear1}</p>
 			<p class="justified mt-0 mb-1" style="font-size: 17px;">${qualification1}</p>
-			<p class="justified mt-0 mb-3" style="font-size: 17px;">${description1}</p>
-        
-            <h3 class="mb-0">${school}</h3>
+			<p class="justified mt-0 mb-3" style="font-size: 17px;">${description1}</p>`;
+
+	if (school != "" && school != null)
+		htmlTemplate += `  <h3 class="mb-0">${school}</h3>
 			<p class="text-muted light mt-0 mb-1">${fromyear2}<span class="mx-2">to</span>${toyear2}</p>
 			<p class="justified mt-0 mb-1" style="font-size: 17px;">${qualification2}</p>
-			<p class="justified mt-0 mb-1" style="font-size: 17px;">${description2}</p>
+			<p class="justified mt-0 mb-1" style="font-size: 17px;">${description2}</p>`;
 
-            <div class="rela-block caps greyed">Experience</div>
-            <h3>${institute1}</h3>
+	htmlTemplate += `<div class="rela-block caps greyed">Experience</div>`;
+
+	if (institute1 != "" && institute1 != null)
+		htmlTemplate += `<h3>${institute1}</h3>
             <p class="text-muted light mt-1 mb-2" style="font-size:17px;">${position1}<span class="ml-5 pl-5">Duration: ${duration1}</span></p>
-            <p class="justified" style="font-size:17px;">${experienceDescription1}</p>
-            <h3>${institute2}</h3>
-            <p class="text-muted light mt-1 mb-2" style="font-size:17px;">${position2}<span class="ml-5 pl-5">Duration: ${duration2}</span></p>
-            <p class="justified" style="font-size:17px;">${experienceDescription2}</p>
+            <p class="justified" style="font-size:17px;">${experienceDescription1}</p>`;
 
-			<div class="rela-block caps greyed">Projects</div>
-            <h3>${title1}</h3>
+	if (institute2 != "" && institute2 != null)
+		htmlTemplate += `<h3>${institute2}</h3>
+            <p class="text-muted light mt-1 mb-2" style="font-size:17px;">${position2}<span class="ml-5 pl-5">Duration: ${duration2}</span></p>
+            <p class="justified" style="font-size:17px;">${experienceDescription2}</p>`;
+
+	htmlTemplate += `<div class="rela-block caps greyed">Projects</div>`;
+
+	if (title1 != "" && title1 != null)
+		htmlTemplate += `
+			<h3>${title1}</h3>
             <p class="light mt-1 mb-2" style="font-size:17px;"><span class="mr-2"><i class="fas fa-link"></i></span>${link1}</p>
-            <p class="justified" style="font-size:17px;">${experienceDescription1}</p>
-            
+            <p class="justified" style="font-size:17px;">${experienceDescription1}</p>`;
+
+	if (title2 != "" && title2 != null)
+		htmlTemplate += `
             <h3>${title2}</h3>
             <p class="light mt-1 mb-2" style="font-size:17px;"><span class="mr-2"><i class="fas fa-link"></i></span>${link1}</p>
             <p class="justified" style="font-size:17px;">${experienceDescription2}</p>
