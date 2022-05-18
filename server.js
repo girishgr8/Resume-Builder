dea0478a3a9800c09c48c209bd34d49691d7594a
girishgr8 <girishthatte35@gmail.com>
2022-05-18 22:03:43 +0530

    Heroku Deployment

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const options = {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "/client")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/client/build", "index.html"));
